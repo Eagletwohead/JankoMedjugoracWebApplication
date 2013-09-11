f688194a10c61a8ee4a3959a3e1ca546f974e576
Eagletwohead <eagletwohead@gmail.com>
2013-09-11 15:45:21 +0200

    Whole application
    Twitter authentification needed

---
+++ JankoMedjugoracWebApplication/Default.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="JankoMedjugoracWebApplication._Default" %>
+
+<asp:Content runat="server" ID="JavaScriptContent" ContentPlaceHolderID="JavaScriptContent">
+
+    <script type="text/javascript">
+        function cleanInputs() {
+            $('input:text').val("");
+        }
+    </script>
+</asp:Content>
+<asp:Content runat="server" ID="FeaturedContent" ContentPlaceHolderID="FeaturedContent">
+    <section class="featured">
+        <div class="content-wrapper">
+            <hgroup class="title">
+                <h1>Numerspiel.</h1>
+                <h2>Preview applicaton.</h2>
+            </hgroup>
+            <p>
+                Preview web applicaton for applying for job as ASP.NET developer. Made by Janko Medjugorac
+            </p>
+        </div>
+    </section>
+</asp:Content>
+<asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
+    <h3>Insert number range:</h3>
+    <table>
+        <tr>
+            <td>
+                <asp:Localize ID="lclStartNumber" runat="server" Text="Start number"></asp:Localize></td>
+            <td>
+                <asp:TextBox ID="txtStartNumber" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvtxtStartNumber" runat="server" ControlToValidate="txtStartNumber"
+                    ErrorMessage="Please enter starting number for interval." Display="Dynamic" ValidationGroup="CalculateValidation"></asp:RequiredFieldValidator>
+                <asp:CompareValidator ID="cmvtxtStartNumber" runat="server" Type="Integer" ControlToValidate="txtStartNumber"
+                    ErrorMessage="Start needs to be whole number." Display="Dynamic" ValidationGroup="CalculateValidation" Operator="DataTypeCheck"></asp:CompareValidator>
+
+            </td>
+
+        </tr>
+        <tr>
+            <td>
+                <asp:Localize ID="lclEndNumber" runat="server" Text="End number"></asp:Localize></td>
+            <td>
+                <asp:TextBox ID="txtEndNumber" runat="server"></asp:TextBox>
+                <asp:RequiredFieldValidator ID="rfvEndNumer" runat="server" ControlToValidate="txtEndNumber"
+                    ErrorMessage="Please enter ending number for interval." Display="Dynamic" ValidationGroup="CalculateValidation"></asp:RequiredFieldValidator>
+                <asp:CompareValidator ID="cmvtxtEndNumber" runat="server" Type="Integer" ControlToValidate="txtEndNumber"
+                    ErrorMessage="End needs to be whole number." Display="Dynamic" ValidationGroup="CalculateValidation" Operator="DataTypeCheck"></asp:CompareValidator>
+                <asp:CompareValidator ID="cmvRange" runat="server" ControlToValidate="txtEndNumber"
+                    ErrorMessage="End needs to be equal or greater then Start." Display="Dynamic" ValidationGroup="CalculateValidation" ControlToCompare="txtStartNumber" Operator="GreaterThanEqual"></asp:CompareValidator>
+            </td>
+        </tr>
+        <tr>
+            <td></td>
+            <td>
+                <asp:Button ID="btnCalculate" runat="server" Text="Calculate" ValidationGroup="CalculateValidation"
+                    CssClass="preview_button" OnClick="btnCalculate_Click" />
+                <asp:Button ID="btnClear" runat="server" Text="Clear" OnClientClick="cleanInputs(); return false;" CssClass="preview_button" /></td>
+        </tr>
+    </table>
+
+    <asp:UpdatePanel runat="server" ID="upnlResults">
+        <ContentTemplate>
+            <asp:ListView ID="lvwResults" runat="server" GroupItemCount="10">
+                <LayoutTemplate>
+                    <table runat="server">
+                        <tr runat="server" id="groupPlaceholder"></tr>
+                    </table>
+                </LayoutTemplate>
+                <GroupTemplate>
+                    <tr runat="server" id="tableRow">
+                        <td runat="server" id="itemPlaceholder" />
+                    </tr>
+                </GroupTemplate>
+                <ItemTemplate>
+                    <td runat="server">
+                        <asp:Label ID="lblResult" runat="server"
+                            Text='<%#Eval("Text") %>' />
+                    </td>
+                </ItemTemplate>
+            </asp:ListView>
+        </ContentTemplate>
+    </asp:UpdatePanel>
+</asp:Content>
---
+++ JankoMedjugoracWebApplication/Default.aspx.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ASPSnippets.TwitterAPI;
+using System.Configuration;
+
+namespace JankoMedjugoracWebApplication
+{
+    public partial class _Default : Page
+    {
+
+#region Constants
+        private const string TWITTER_POST = "https://api.twitter.com/1.1/statuses/update.json";
+
+        //static string API_Key = 
+#endregion
+
+        #region "Page_Load"
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            TwitterConnect.API_Key = System.Configuration.ConfigurationManager.AppSettings["API_Key"];
+            TwitterConnect.API_Secret = System.Configuration.ConfigurationManager.AppSettings["API_Secret"];
+            if (!IsPostBack)
+            {
+                //if (TwitterConnect.IsAuthorized)
+                //{
+                //    btnAuthorize.Enabled = false;
+                //    pnlTweet.Enabled = true;
+                //}
+                if (TwitterConnect.IsDenied)
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "key", "alert('User has denied access.')", true);
+                }
+            }
+        }
+        #endregion
+
+        #region "btnCalculate_Click"
+        /// <summary>
+        /// Handles clicking event on Calculate button. This will initiate result calculation.
+        /// </summary>
+        /// <param name="sender">btnCalculate button.</param>
+        /// <param name="e">Event args. Not used here.</param>
+        /// <remarks>This method assumes that correct data is entered in text inputs because of 
+        /// ASP.NET valudation that is done by validator controls.</remarks>
+        protected void btnCalculate_Click(object sender, EventArgs e)
+        {
+            // Get valid integers values from inputs and get result list:
+            int start = int.Parse(this.txtStartNumber.Text);
+            int end = int.Parse(this.txtEndNumber.Text);
+            List<string> result = GetResults(start, end);
+
+            // Prepare data source for result list view. 
+            // We create objects of anonymus type with one 'Text' property that will be used to bind our result data to result list view:
+            var dataSource = (from r in result
+                              select new { Text = r}).ToList();
+
+            this.lvwResults.DataSource = dataSource;
+            this.lvwResults.DataBind();
+
+            if (result.Count > 0)
+            {
+                TwittResults(string.Join(",", result));
+            }
+            
+        }
+        #endregion
+
+#region GetResults
+        /// <summary>
+        /// Gets list of all the items that needs to be displayed on page as result list.
+        /// </summary>
+        /// <param name="start">Starting point of interval.</param>
+        /// <param name="end">Ending point of interval.</param>
+        /// <returns>List of result text items.</returns>
+        private List<string> GetResults(int start, int end)
+        {
+            List<string> result = new List<string>();
+
+            // For all numbers in given interval, check if they are multiples of 3 or/and 5 and 
+            // prepare appropriate output text:
+            for (int i = start; i < end + 1; i++)
+            {
+                string output = string.Empty;
+                if (i % 3 == 0)
+                {
+                    output = "Fizz";
+                }
+                if (i % 5 == 0)
+                {
+                    output += "Buzz";
+                }
+                else if (i % 3 != 0)
+                {
+                    output =  i.ToString(CultureInfo.CurrentUICulture);
+                }
+
+                result.Add(output);
+            }
+
+            return result;
+        } 
+#endregion
+
+#region TwittResults
+        /// <summary>
+        /// Twitts passed message to Twitter.
+        /// </summary>
+        /// <param name="message">Text message that needs to be twitted.</param>
+        private void TwittResults(string message)
+        {
+            TwitterConnect twitter = new TwitterConnect();
+            if (!TwitterConnect.IsAuthorized)
+            {
+                twitter.OAuthToken = "352481636-LLfi8s5UgTG3wgNaTAAM4PWhebHMZYZJv1pk5XIB";
+                twitter.OAuthTokenSecret = "cXK4IQn1YrJ2JUG4WXq9AaO74f888Nm2OIWfjORo";
+                twitter.Authorize(Request.Url.AbsoluteUri.Split('?')[0]);
+            }
+            // Post results to Twitter. Only 140 characters can be posted to Twitter:
+            string twitt = message.Substring(0,140);
+            twitter.Tweet(twitt);
+        }
+#endregion
+
+    }
+}
